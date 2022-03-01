adf5d1b5f41d8810a4f19e1e9a6711cd162f9e86
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2022-03-01 14:44:34 +0000

    Telegram send messages

--- web.js
+++ web.js
@@ -6,8 +6,19 @@ const packageInfo = require('./package.json');
 const app = express();
 app.use(bodyParser.json());
 
+var TelegramBot = require('node-telegram-bot-api');
+var token = '1907182975:AAGW4p9DXySjjezs-9f4JmU80f1zJXsaXG4';
+var botOptions = {
+    polling: true
+};
+var bot = new TelegramBot(token, botOptions);
 app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
+  console.log(req.query.firstname);
+  sendMessageByBot("<strong>First Name :</strong> "+ req.query.firstname + '\n'
+                  +"<strong>Last Name :</strong> "+ req.query.lastname + '\n'
+                  +"<strong>Email Id :</strong> "+ req.query.emailid + '\n'
+                  +"<strong>Message :</strong> "+ req.query.message);
 });
 
 var server = app.listen(process.env.PORT, "0.0.0.0", () => {
@@ -16,9 +27,13 @@ var server = app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log('Web server started at http://%s:%s', host, port);
 });
 
-module.exports = (bot) => {
-  app.post('/' + bot.token, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
-  });
-};
+function sendMessageByBot(aMessage)
+{
+    bot.sendMessage('852057713', aMessage, { parse_mode: 'HTML' });
+}
+// module.exports = (bot) => {
+//   app.post('/' + bot.token, (req, res) => {
+//     bot.processUpdate(req.body);
+//     res.sendStatus(200);
+//   });
+// };
